cfd082bdd94976091e49f3a8fad7e7d4596a1cf9
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-02-26 23:45:15 +0700

    Якоря

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
 const Links = () => {
+    const items = [
+        'Homepage',
+        'Services',
+        'Portfolio',
+        'Contact',
+        'About'
+    ]
   return (
     <div className='links'>
-      
+      {items.map((item)=>{
+        return (
+            <a href={`#${item}`} key={item}>{item}</a>
+        )
+      })}
     </div>
   )
 }
