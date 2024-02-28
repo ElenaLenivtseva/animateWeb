8953e6360c50824caffb36dc548d77b881afb0cd
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-02-28 21:50:21 +0700

    Форма с анимацией svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./app.scss";
+import Contact from "./components/contact/Contact";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
@@ -20,7 +21,7 @@ const App = () => {
         <Parallax type="portfolio" />
       </section>
       <Portfolio/>
-      <section id="Contact">Contact</section>
+      <section id="Contact"><Contact/></section>
     </div>
   );
 };
