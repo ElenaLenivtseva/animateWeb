ed2af1477d18d9411e5ee96022ef5a0ebe3e7e77
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-02-28 20:02:19 +0700

    анимация Services - при наведении и при скролле

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,32 +1,57 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Services.scss";
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 import Box from "./Box";
 
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transitin: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
 const Services = () => {
+  const ref = useRef();
+  const isInView = useInView(ref, { margin: "-100px" });
   return (
-    <motion.div className="services">
+    <motion.div
+      className="services"
+      variants={variants}
+      initial="initial"
+      // whileInView='animate'
+      ref={ref}
+      animate={isInView && 'animate'}
+    >
       <motion.div className="textContainer">
         <motion.p>
           I focus on helping your brand grow <br /> and move forwardly
         </motion.p>
         <hr />
       </motion.div>
-      <motion.div className="titleContainer">
+      <motion.div className="titleContainer" variants={variants}>
         <div className="title">
           <img src="/people.webp" alt="" />
           <h1>
-            <b>Unique</b> Ideas
+            <motion.b whileHover={{color: 'orange'}}>Unique</motion.b> Ideas
           </h1>
         </div>
         <div className="title">
           <h1>
-            <b>For Your</b> Business
+            <motion.b whileHover={{color: 'orange'}}>For Your</motion.b> Business
           </h1>
           <button>WHAT WE DO?</button>
         </div>
       </motion.div>
-      <motion.div className="listContainer">
+      <motion.div className="listContainer" variants={variants}>
         <Box />
         <Box />
         <Box />
